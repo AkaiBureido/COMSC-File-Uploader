b9e215370fcd24e8df6250d67d73debfff618d19
Oleg Utkin <utkin.oleg@me.com>
2012-09-08 14:56:34 -0700

    Refined file scraping. Getting Ready to start fiddling with file uploading.

--- COMSC Batch Uploader/main.m
+++ COMSC Batch Uploader/main.m
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+NSArray* scrapeFiles(NSString *path, NSArray *acceptableExtensions);
+
 int main(int argc, const char * argv[])
 {
 
@@ -20,31 +22,88 @@ int main(int argc, const char * argv[])
        
             //test setup r
         
-        const int testArgc = 1;
+        const int testArgc = 3;
         NSString* testArgv[testArgc];
-        testArgv[0] = [[NSString alloc] initWithFormat:@"/Users/olegforzane/Desktop/"];
         
-        NSFileManager* fileManager = [NSFileManager defaultManager];
-        BOOL isDirectory = false;
-        BOOL itemExists = [fileManager fileExistsAtPath:testArgv[0] isDirectory:&isDirectory];
-        
-        if (itemExists == true) {
-            NSLog(@"It exists.");
-            if (isDirectory == true) {
-                NSLog(@"It is a directory.");
-                
-                    //TODO: scrape all visible files 0 level deep and upload
-                
-            } else {
-                NSLog(@"It is a file.");
-                
-                    //TODO: upload
-            }
-        } else {
-            NSLog(@"It does not exist.");
-        }
+        testArgv[0] = [[NSString alloc] initWithFormat:@"/Users/olegforzane/Clouds/SkyDrive/Developer_Cloud/College/Comsc210_Workspace/Comsc210_Homework/Lab 2b (Rectangle)/Lab 2b (Rectangle)"];
+        testArgv[1] = [[NSString alloc] initWithFormat:@"/Users/olegforzane/Clouds/SkyDrive/Developer_Cloud/College/Comsc210_Workspace/Comsc210_Homework/Lab 3a (Road)/Lab 3a (Road)"];
+        testArgv[2] = [[NSString alloc] initWithFormat:@"/Users/olegforzane/Clouds/SkyDrive/Developer_Cloud/College/Comsc210_Workspace/Comsc210_Homework/Lab 2b (Rectangle)/Lab 2b (Rectangle)"];
 
+        
+        NSArray *acceptableExtensions = [[NSArray alloc] initWithObjects: @"cpp",
+                                                                          @"h", nil];
+        
+        
+        
+        NSMutableArray *filesToUpload = [[NSMutableArray alloc] init];
+        
+        for ( int iterator = 0; iterator < testArgc; iterator++ ) {
+            NSArray *currentScrapedFiles;
+            @try {
+                 currentScrapedFiles = scrapeFiles(testArgv[iterator], acceptableExtensions);
+            }
+            @catch (NSException *exception) {
+                NSLog(@"%@: %@",[exception name], [exception description]);
+            }
+            NSString* filePath;
+            for (filePath in currentScrapedFiles) {
+                if (![filesToUpload containsObject:filePath]) {
+                    [filesToUpload addObject:filePath];
+                }
+            }
+        }
+        
+        NSLog(@"%@", filesToUpload);
+                
     }
     return 0;
 }
 
+
+NSArray* scrapeFiles(NSString *path, NSArray *acceptableExtensions) {
+    NSMutableArray *filesToUpload = [[NSMutableArray alloc] init];
+    
+    NSFileManager* fileManager = [NSFileManager defaultManager];
+    
+    BOOL isDirectory = false;
+    BOOL itemExists = [fileManager fileExistsAtPath:path isDirectory:&isDirectory];
+    
+    if (itemExists == true) {
+            //NSLog(@"It exists.");
+        if (isDirectory == true) {
+                //NSLog(@"It is a directory.");
+            
+            NSDirectoryEnumerator *dir = [fileManager enumeratorAtPath:path];
+            
+            NSMutableString *filePath;
+            while (filePath = [dir nextObject]) {
+                if ( [dir level] == 1 ) [dir skipDescendents];
+                    
+                    //Creating complete filepath:
+                filePath = [NSString stringWithFormat:@"%@/%@",path, filePath];
+                NSString *fileName = [fileManager displayNameAtPath:filePath];
+                
+                    //Eliminating hidden files:
+                if ([fileName characterAtIndex:0] != '$' && [fileName characterAtIndex:0] != '.') {
+                    if (acceptableExtensions == nil) {
+                            //Adding all found files
+                        [filesToUpload addObject:filePath];
+                    } else {
+                            //Filtering Files based on extensions
+                        if( [acceptableExtensions containsObject:[filePath pathExtension]] ) {
+                            [filesToUpload addObject:filePath];
+                        }
+                    }
+                }   
+            }
+        } else {
+                //NSLog(@"It is a file.");
+            [filesToUpload addObject:path];
+        }
+    } else {
+            //NSLog(@"It does not exist.");
+        [NSException raise:@"NoSuchFile" format:@"File or directory not found"];
+    }
+    
+    return filesToUpload;
+}
